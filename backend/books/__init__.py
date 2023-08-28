755e2215eb51c7754ce14a87f403df1eb13509d5
gustavolobosa <galobos@miuandes.cl>
2023-08-28 02:53:30 -0400

    autopopulate

--- backend/books/__init__.py
+++ backend/books/__init__.py
@@ -1,6 +1,9 @@
+import csv
+import os
 from flask import Flask
 from flask import render_template, url_for
 from werkzeug.middleware.proxy_fix import ProxyFix
+from backend.books.models import Rating
 from flask_alembic import Alembic
 from .db import db
 from books.models import Author, Book
@@ -52,5 +55,56 @@ def create_app():
                 'average_score': 0 if len(book.ratings) == 0 else sum(map(lambda x: x.score, book.ratings))/len(book.ratings)
             })
         return render_template('books.html', books=books_dicts)
-
-    return app
+    
+    def populate_db():
+        script_directory = os.path.dirname(__file__)
+        db_file_folder = os.path.join(script_directory, 'db_files')
+        authors_csv_path = os.path.join(db_file_folder, 'authors.csv')
+        books_csv_path = os.path.join(db_file_folder, 'books.csv')
+        ratings_csv_path = os.path.join(db_file_folder, 'ratings.csv')
+        
+        with app.app_context():
+            try:
+                with open(authors_csv_path, 'r') as authors_file:
+                    authors_reader = csv.DictReader(authors_file, delimiter=';')
+                    for row in authors_reader:
+                        try:
+                            author = Author(name=row['name'], openlibrary_id=row['key'])
+                            db.session.add(author)
+                        except Exception as e:
+                            print("Wrong key for row: ", row)
+                db.session.commit()
+                
+                with open(books_csv_path, 'r') as books_file:
+                    books_reader = csv.DictReader(books_file, delimiter=';')
+                    for row in books_reader:
+                        try:
+                            author_id = row['author']
+                            if author_id.startswith('/authors/'):
+                                author = Author.query.filter_by(openlibrary_key=author_id).first()
+                                if author:
+                                    author_id = author.id
+                                else:
+                                    print(f"author not found for key: {author_id}", flush=True)
+                                    continue
+                            
+                            book = Book(title=row['title'], openlibrary_key=row['key'], author_id=author_id, description=row['description'])
+                            db.session.add(book)
+                            
+                        except Exception as e:
+                            print("Wrong key for row: ", row)
+                            
+                db.session.commit()
+                
+                with open(ratings_csv_path, 'r') as ratings_file:
+                    ratings_reader = csv.DictReader(ratings_file, delimiter=';')
+                    for row in ratings_reader:
+                        book = Book.query.filter_by(openlibrary_key=row['book']).first()
+                        rating = Rating(book_id=book.id, score=row['score'])
+                        db.session.add(rating)
+                        
+                db.session.commit()
+            except Exception as e:
+                print("An error occurred while populating the database: ", e)
+    
+    populate_db()
